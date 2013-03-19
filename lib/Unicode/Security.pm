b977e67e6295c9877f69ae6865a0d0c33ebe7633
Nick Patch <patch@cpan.org>
2013-03-18 23:04:03 -0400

    build out new module

---
+++ lib/Unicode/Security.pm
@@ -0,0 +1,59 @@
+package Unicode::Security;
+
+use v5.6;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.00_1';
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Unicode::Security - ...
+
+=head1 VERSION
+
+This document describes Unicode::Security version 0.00_1.
+
+=head1 SYNOPSIS
+
+    use Unicode::Security;
+
+    ...
+
+=head1 DESCRIPTION
+
+Unicode::Security is a Perl 5 module implementing detection mechanisms from
+Unicode Technical Standard #39: Unicode Security Mechanisms.
+
+=head1 SEE ALSO
+
+=over
+
+=item * Unicode Technical Standard #39: Unicode Security Mechanisms -
+L<http://www.unicode.org/reports/tr39/>
+
+=item * Unicode Technical Report #36: Unicode Security Considerations -
+L<http://www.unicode.org/reports/tr36/>
+
+=item * Unicode FAQ: Security Issues -
+L<http://www.unicode.org/faq/security.html>
+
+=back
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2013 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More tests => 1;
+
+BEGIN { use_ok 'Unicode::Security' }
+
+diag "Testing Unicode::Security $Unicode::Security::VERSION, Perl $], $^X";
